b2e77cb09c2177b35d9c592552f3ced7b3fb1b98
Sakshi Jadhav <jadhavsakshi3495@gmail.com>
2025-12-14 18:04:40 +0530

    Add files via upload

---
+++ Program30_5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Program30_5
+{
+    public static void Pattern(int iRow, int iCol)
+    {
+        for(int i = 1; i <= iRow; i++)
+        {
+            for(int j = 1; j <= iCol; j++)
+            {
+                if(i == 1 || i == iRow)           
+                    System.out.print(j + "\t");
+                else if(j == 1 || j == iCol)      
+                    System.out.print(j + "\t");
+                else
+                    System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+        System.out.println("Enter number of rows and columns:");
+        iValue1 = sc.nextInt();
+        iValue2 = sc.nextInt();
+
+        Pattern(iValue1, iValue2);
+    }
+}
+
